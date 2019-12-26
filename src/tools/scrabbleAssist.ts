e565b48f9f796b86330d5cd9c570ada96afd43c0
Colin Turner <colinturner34@gmail.com>
2019-12-25 22:32:12 -0500

    feat(scrabbleAssist): do not check letters more than once

--- src/tools/scrabbleAssist.ts
+++ src/tools/scrabbleAssist.ts
@@ -1,6 +1,17 @@
+interface LettersAlreadyReviewed {
+  [key: string]: boolean;
+}
+
 const scrabbleAssist = (letters: string[], words: string[]) => {
   return words.filter(word => {
+    const lettersAlreadyReviewed: LettersAlreadyReviewed = {};
     return letters.every(letter => {
+      if (lettersAlreadyReviewed[letter]) {
+        return true;
+      } else {
+        lettersAlreadyReviewed[letter] = true;
+      }
+
       const letterUnderReview = letter;
       const numberOfOccurrencesOfLetterInLetters = letters.filter(
         (letter: string) => letter === letterUnderReview

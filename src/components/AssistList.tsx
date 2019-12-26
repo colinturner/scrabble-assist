012bf860bf11636f1425a9ff6a6d176128028444
Colin Turner <colinturner34@gmail.com>
2019-12-26 15:34:15 -0500

    feat(AssistList): add styles

--- src/components/AssistList.tsx
+++ src/components/AssistList.tsx
@@ -3,33 +3,72 @@ import scrabbleAssist from "../tools/scrabbleAssist";
 import ENGLISH_WORDS from "../constants/englishWords";
 import styled from "styled-components";
 
-const Inputs = styled.div`
+const Container = styled.div`
+  padding: 20px;
+  width: fit-content;
+`;
+
+const Title = styled.div`
+  font-size: 35px;
+`;
+
+const Header = styled.div`
   display: flex;
-  background: red;
+  flex-direction: column;
+  padding: 20px 0px;
+`;
+
+const InputGroup = styled.div`
+  display: flex;
+  :not(:last-child) {
+    padding-bottom: 20px;
+  }
+`;
+
+const Label = styled.div`
+  background: aliceblue;
+  padding: 10px;
+  margin-right: 10px;
+  border-radius: 3px;
+`;
+
+const Input = styled.input`
+  font-size: 20px;
+  border-radius: 3px;
+`;
+
+const List = styled.div`
+  white-space: pre-line;
+  line-height: 30px;
 `;
 
 const AssistList = () => {
   const [letters, setLetters] = useState("");
   const [lettersLonger, setLettersLonger] = useState(NaN);
   return (
-    <>
-      <Inputs>
-        <label>Letters in your hand:</label>
-        <input placeholder="xyz" onChange={e => setLetters(e.target.value)} />
-        <label>Words with this many extra letters:</label>
-        <input
-          type="number"
-          onChange={e => setLettersLonger(parseInt(e.target.value))}
-        />
-      </Inputs>
-      <div style={{ whiteSpace: "pre-line", lineHeight: "30px" }}>
+    <Container>
+      <Title>Scrabble Assist</Title>
+      <Header>
+        <InputGroup>
+          <Label>Letters in your hand:</Label>
+          <Input placeholder="xyz" onChange={e => setLetters(e.target.value)} />
+        </InputGroup>
+        <InputGroup>
+          <Label>Find words with this many extra letters:</Label>
+          <Input
+            type="number"
+            onChange={e => setLettersLonger(parseInt(e.target.value))}
+          />
+        </InputGroup>
+      </Header>
+      <List>
         {scrabbleAssist({
           letters: letters.split(""),
           words: ENGLISH_WORDS,
           lettersLonger
         }).join("\r\n")}
-      </div>
-    </>
+      </List>
+    </Container>
   );
 };
 

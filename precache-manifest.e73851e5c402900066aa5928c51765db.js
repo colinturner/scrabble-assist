9ce9efb4eb6fecdf4a70c9f72bfcca8a7e4e7206
Colin Turner <colinturner34@gmail.com>
2019-12-26 17:27:48 -0500

    Updates

---
+++ precache-manifest.e73851e5c402900066aa5928c51765db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "629bb5651f11f36ce921210a57274ed7",
+    "url": "/scrabble-assist/index.html"
+  },
+  {
+    "revision": "a58fe27aaa99dac29b42",
+    "url": "/scrabble-assist/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3beed7d2ea7db0f2f03e",
+    "url": "/scrabble-assist/static/js/2.afe2bbcf.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/scrabble-assist/static/js/2.afe2bbcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a58fe27aaa99dac29b42",
+    "url": "/scrabble-assist/static/js/main.794555fb.chunk.js"
+  },
+  {
+    "revision": "fd71ae0fd259decc6df1",
+    "url": "/scrabble-assist/static/js/runtime-main.1b0c0956.js"
+  }
+]);
